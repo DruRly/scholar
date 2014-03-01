53a4eacdb02c87a88cef8f6d3c451955312a8bdf
Dru Riley <drurly@gmail.com>
2014-03-01 14:10:11 -0500

    Finalize Form

--- scholar.js
+++ scholar.js
@@ -1,19 +1,50 @@
+//if (Meteor.isClient) {
+//Template.hello.greeting = function () {
+//return "Welcome to scholar.";
+//};
+
+//Template.hello.events({
+//'click input': function () {
+//template data, if any, is available in 'this'
+//if (typeof console !== 'undefined')
+//console.log("You pressed the button");
+//}
+//});
+//}
+var Volunteers = new Meteor.Collection('volunteers');
+
+
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to scholar.";
-  };
+  Template.hello.events({'submit form' : function(event, template) {
+    event.preventDefault();
 
-  Template.hello.events({
-    'click input': function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button");
-    }
-  });
-}
+    name = template.find("input[name=name]");
+    email = template.find("input[name=email_address]");
+    linked_in = template.find("input[name=linked_in]");
+    phone_number = template.find("input[name=phone_number]");
+    area_of_interest = template.find("input[name=area_of_interest]");
+    why = template.find("textarea[name=why]");
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
+    var data = {
+      name: name.value,
+      email: email.value,
+      linked_in: linked_in.value,
+      phone_number: phone_number.value,
+      area_of_interest: area_of_interest.value,
+      why: why.value
+    };
+
+    email.value="";
+    firstname.value="";
+    lastname.value="";
+
+      name.value=""
+      email.value=""
+      linked_in.value=""
+      phone_number.value=""
+      area_of_interest.value=""
+      why.value=""
+
+    Volunteers.insert(data, function(err) { /* handle error */ });
+  }});
 }
